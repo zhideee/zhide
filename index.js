59c19104bf94b7728e95d6b16668a400a16a9e5d
zhideee <59455175+zhideee@users.noreply.github.com>
2020-09-11 20:21:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ let button = document.getElementById("codeA");
 
 button.onclick = function(){
 
-    if (prompt("Gib hier deinen Code ein:") === "187") {
+    var input = prompt("Gib hier deinen Code ein:") 
+
+    if (input === "ygc") {
         window.location.href = "https://drive.google.com/uc?export=download&id=1-wB4OzGxpxaSodkbf15t1DjLowBMXUab";
     }
     else{
-        alert("Dieser Code existiert nicht oder wurde falsch eingegeben!")
+        if (input === "187") {
+            document.getElementById("nggyu").play();
+        }
+        else{
+            if(input === "paypal"){
+                window.location.href = "https://paypal.me/zhideee";
+            }
+            else{
+                alert("Dieser Code existiert nicht oder wurde falsch eingegeben!")
+            }
+        }
     }
-
 }
