b6c796c1ea8b7960bb2356ac9745b00059b76721
zhideee <59455175+zhideee@users.noreply.github.com>
2020-09-11 20:29:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ button.onclick = function(){
             document.getElementById("nggyu").play();
         }
         else{
-            if(input === "paypal"){
+            if(input === "paypal" || "PayPal" || "payPal" || "Paypal" || "PAYPAL"){
                 window.location.href = "https://paypal.me/zhideee";
             }
             else{
